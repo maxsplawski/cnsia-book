db01ef7aeac4ca69a6336dc184c3e9dd455c3a8e
Max Spławski <mniamek9@gmail.com>
2024-11-23 15:08:25 +0100

    feat: add flyway

--- chapter05/catalog-service/build.gradle.kts
+++ chapter05/catalog-service/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	implementation("org.springframework.retry:spring-retry")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
